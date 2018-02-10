360d47a57892ddfdef0b6efccfe8e7580a0672c8
zelin <zelin.xiong@ctgtmo.com>
2018-02-10 20:46:40 +0800

    添加main方法到GitHub中

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("hello world!");
+        int result = addUser(9);
+    }
+
+    public int deleteUser(int i) {
+        return 0;
+    }
+
+
+    private static int addUser(int i) {
+        System.out.println("hello world!");
+        return 0;
+    }
+}
