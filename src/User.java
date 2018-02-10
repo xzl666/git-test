f07dac4ec74431da6842b23021c1a31a8e6a7b99
zelin <zelin.xiong@ctgtmo.com>
2018-02-10 22:17:34 +0800

    第一次编辑temp这个文件

---
+++ src/User.java
@@ -0,0 +1,7 @@
+public class User {
+    public static void main(String[] args) {
+        System.out.println("I want to add a new function!");
+
+        System.out.println("I want to add a new method!");
+    }
+}
